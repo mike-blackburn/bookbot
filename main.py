a426bc1272f3c74526985e2352fc4213979ed601
Mike Blackburn <mjblackburn1973@gmail.com>
2025-10-26 19:06:44 -0500

    finish report formatting

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
-from stats import get_num_words, get_num_characters
+from stats import get_num_words, get_num_characters, get_sorted_list
 
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
+def format_report(path, num_words, sorted_list):
+    print("======== BOOKBOT ========")
+    print(f"Analyzing book found at {path}...")
+    print("------- Word Count ------")
+    print(f"Found {num_words} total words")
+    print("---- Character Count ----")
+    for item in sorted_list:
+        print(f"{item['char']}: {item['num']}")
+
+
+    print("========== END ==========")
+
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     num_chars_dict = get_num_characters(text)
-    print(f"Found {num_words} total words")
-    for char in num_chars_dict:
-        print(f"{char}: {num_chars_dict[char]}")
+    sorted_dict_list = get_sorted_list(num_chars_dict)
+    # print(f"Found {num_words} total words")
+    # for char in num_chars_dict:
+    #     print(f"{char}: {num_chars_dict[char]}")
+    # print(sorted_dict_list)
+    format_report(book_path, num_words, sorted_dict_list)
 
 main()
--- stats.py
+++ stats.py
@@ -6,9 +6,23 @@ def get_num_words(text):
 def get_num_characters(text):
     char_dict = {}
     for char in text:
-        normalized_char = f"'{char.lower()}'"
+        # normalized_char = f"'{char.lower()}'"
+        normalized_char = char.lower()
         if normalized_char not in char_dict:
             char_dict[normalized_char] = 1
         else:
             char_dict[normalized_char] += 1
     return char_dict
+
+def sort_on_helper(items):
+    return items["num"]
+def get_sorted_list(dict_to_sort):
+    # print(dict_to_sort)
+    sorted_list = []
+    for key, value in dict_to_sort.items():
+        if key.isalpha():
+            sorted_list.append({"char": key, "num": value})
+
+
+    sorted_list.sort(reverse=True, key=sort_on_helper)
+    return sorted_list
