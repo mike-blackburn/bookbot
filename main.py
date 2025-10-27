282763972db246a1ba6cd71820ef19eff6538a8d
Mike Blackburn <mjblackburn1973@gmail.com>
2025-10-26 19:51:14 -0500

    add sys.argv usage

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+import sys
 from stats import get_num_words, get_num_characters, get_sorted_list
 
+def main():
+    book_path = get_book_path()
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    num_chars_dict = get_num_characters(text)
+    sorted_dict_list = get_sorted_list(num_chars_dict)
+    # print(f"Found {num_words} total words")
+    # for char in num_chars_dict:
+    #     print(f"{char}: {num_chars_dict[char]}")
+    # print(sorted_dict_list)
+    format_report(book_path, num_words, sorted_dict_list)
+
+def get_book_path():
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    return sys.argv[1]
 
 def get_book_text(path):
     with open(path) as f:
@@ -16,17 +34,5 @@ def format_report(path, num_words, sorted_list):
 
 
     print("========== END ==========")
-
-def main():
-    book_path = "books/frankenstein.txt"
-    text = get_book_text(book_path)
-    num_words = get_num_words(text)
-    num_chars_dict = get_num_characters(text)
-    sorted_dict_list = get_sorted_list(num_chars_dict)
-    # print(f"Found {num_words} total words")
-    # for char in num_chars_dict:
-    #     print(f"{char}: {num_chars_dict[char]}")
-    # print(sorted_dict_list)
-    format_report(book_path, num_words, sorted_dict_list)
 
 main()
